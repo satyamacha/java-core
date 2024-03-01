51b56f71cae8e37ff3fe5b6211c103212ca4ba40
satyanarayana bypureddy <bypureddysatyanarayana@gmail.com>
2024-03-01 23:45:31 +0530

    files added

---
+++ src/Day01/D01P01.java
@@ -0,0 +1,46 @@
+package Day01;
+
+class Book {
+    private String bookTitle;
+    private double bookPrice;
+
+    // Get for bookTitle
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    // Set for bookTitle
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    // Get for bookPrice
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    // Set for bookPrice
+    public void setBookPrice(double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+}
+
+public class D01P01 {
+    public static Book createBooks(String title, double price) {
+        Book book = new Book();         // create a object for book
+        book.setBookTitle(title);
+        book.setBookPrice(price);
+        return book;
+    }
+
+    // displaying book information
+    public static void showBooks(Book book) {
+        System.out.println("Book Title: " + book.getBookTitle() + " and Price: " + book.getBookPrice());
+    }
+
+  
+    public static void main(String[] args) {
+        Book javaProgrammingBook = createBooks("Java Programming", 350.00);
+        showBooks(javaProgrammingBook);
+    }
+}
